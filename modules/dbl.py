670672db17e8a8ad2f6d679f729b3efb5af28bfd
ReKT <mineblock20@gmail.com>
2018-02-27 14:51:05 +1100

    fixed shard ids

--- modules/dbl.py
+++ modules/dbl.py
@@ -16,7 +16,7 @@ class DiscordBotsOrgAPI:
         while True:
             print("Attempting to update server count.")
             try:
-                await self.dblpy.post_server_count(shard_count=self.shard_count, shard_no=self.shard_id)
+                await self.dblpy.post_server_count(shard_count=self.bot.shard_count, shard_no=self.bot.shard_id)
                 print("Posted server count. {}".format(len(self.bot.guilds)))
             except Exception as e:
                 print('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
