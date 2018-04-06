a957773752a1b6587922ede75cf8c0dc16e59495
ReKT <rektdiscord@gmail.com>
2018-04-07 06:42:06 +1000

    OwO

--- modules/discordbots.py
+++ modules/discordbots.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
-import asyncio, config, dbl, discord, random
+import asyncio, config, dbl, discord, random, aiohttp
 
 messages = ["OwO Whats this", "MonkaS", "OwO", "Haiiiii", ".help", "🤔🤔🤔", "HMMM🤔", "USE n! WEW", "n!HELP REE"]
+stats2 = [f"Servers: {len(self.bot.guilds)}", f"Users: {len(set(self.bot.get_all_members()))}", "OwO whats n!help", "🤔🤔🤔"]
 
 class DiscordBotsOrgAPI:
     """Handles interactions with the discordbots.org API"""
@@ -18,15 +19,33 @@ class DiscordBotsOrgAPI:
         while True:
             print("Attempting to update server count.")
             try:
-                stats2 = [f"Servers: {len(self.bot.guilds)}",
-                          f"Users: {len(set(self.bot.get_all_members()))}",
-                          "OwO whats n!help", "🤔🤔🤔"]
                 await self.dblpy.post_server_count(shard_count=self.bot.shard_count, shard_no=self.bot.shard_id)
                 print("Posted server count. {}".format(len(self.bot.guilds)))
                 game = discord.Streaming(name=random.choice(stats2), url="https://www.twitch.tv/rekt4lifecs")
                 await self.bot.change_presence(activity=game)
             except Exception as e:
                 print('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post('https://bots.discord.pw/api/bots/310039170792030211/stats',
+                                            headers={'Authorization': f'{config.dpw_key}'},
+                                            data={"shard_count": self.bot.shard_count,
+                                                  "server_count": len(self.bot.guilds)}) as response:
+                        t = await response.json()
+                        print(t)
+            except Exception as e:
+                print(f"Failed to post to pw, {e}")
+
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post('https://ls.terminal.ink/api/v1/bots/310039170792030211',
+                                            headers={'Authorization': f'{config.terminal_key}'},
+                                            data={"server_count": len(self.bot.guilds)}) as response:
+                        t = await response.json()
+                        print(t)
+            except Exception as e:
+                print(f"Failed to post to terminal, {e}")
             await asyncio.sleep(1800)
 
 def setup(bot):
