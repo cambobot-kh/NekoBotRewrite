83f0c4be675d615a0cfd10384a9aff624b693628
ReKT <rektdiscord@gmail.com>
2018-04-17 01:32:06 +1000

    Discriminator Info + Update Help

--- modules/general.py
+++ modules/general.py
@@ -8,8 +8,27 @@ from PIL import __version__ as pilv
 from bs4 import __version__ as bsv
 from urllib.parse import quote_plus
 import string, json
-
+from .utils.paginator import EmbedPages, Pages
+from scipy import stats
+import numpy
 from .utils.paginator import HelpPaginator
+
+class Discriminator(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            if not int(argument) in range(1, 10000):
+                raise commands.BadArgument('That isn\'t a valid discriminator.')
+        except ValueError:
+            raise commands.BadArgument('That isn\'t a valid discriminator.')
+        else:
+            return int(argument)
+
+
+class Selector(commands.Converter):
+    async def convert(self, ctx, argument):
+        if argument not in ['>', '>=', '<', '<=', '=']:
+            raise commands.BadArgument('Not a valid selector')
+        return argument
 
 def millify(n):
     millnames = ['', 'k', 'M', ' Billion', ' Trillion']
@@ -568,6 +587,98 @@ class General:
                                                                                       f"```"))
 
     @commands.command()
+    async def discriminfo(self, ctx):
+        """Get some stats about the servers discrims"""
+        discrim_list = [int(u.discriminator) for u in ctx.guild.members]
+
+        # The range is so we can get any discrims that no one has.
+        # Just subtract one from the number of uses.
+        count = Counter(discrim_list + [int(i) for i in range(1, 10000)])
+        count = sorted(count.items(), key=lambda c: c[1], reverse=True)
+
+        embeds = {
+            'Summary': {
+                'Most Common': ', '.join(str(d[0]) for d in count[:3])
+                               + ', and ' + str(count[4][0]),
+                'Least Common': ', '.join(str(d[0]) for d in count[-4:-1][::-1])
+                                + ', and ' + str(count[-1][0]),
+                'Three Unused': '\n'.join([str(d[0]) for d in count
+                                           if d[1] == 1][:3]),
+                'Average': numpy.mean(discrim_list),
+            },
+            'Statistics': {
+                'Average': numpy.mean(discrim_list),
+                'Mode': stats.mode(discrim_list)[0][0],
+                'Median': numpy.median(discrim_list),
+                'Standard Deviation': numpy.std(discrim_list),
+            }
+        }
+
+        final_embeds = []
+
+        for embed_title in embeds.keys():
+            e = discord.Embed(title=embed_title)
+            for field_name in embeds[embed_title].keys():
+                e.add_field(name=field_name,
+                            value=embeds[embed_title][field_name], inline=False)
+            final_embeds.append(e)
+
+        p = EmbedPages(ctx, embeds=final_embeds)
+        await p.paginate()
+
+    # It's a converter, not a type annotation in this case
+    # noinspection PyTypeChecker
+    @commands.command()
+    async def discrim(self, ctx, discriminator: Discriminator = None,
+                      *, selector: Selector = '='):
+        """Search for specific discriminators.
+
+        Optional parameter for ranges to be searched.
+
+        It can be >, >=, <=, or <.
+
+        Ranges between two numbers hasn't been implemented yet."""
+        if not discriminator:
+            discriminator = int(ctx.author.discriminator)
+        if selector == '>':
+            p = Pages(ctx, entries=[
+                f'{u.display_name}#{u.discriminator}'
+                for u in ctx.guild.members
+                if int(u.discriminator) > discriminator
+            ])
+        elif selector == '<':
+            p = Pages(ctx, entries=[
+                f'{u.display_name}#{u.discriminator}'
+                for u in ctx.guild.members
+                if int(u.discriminator) < discriminator
+            ])
+        elif selector == '>=':
+            p = Pages(ctx, entries=[
+                f'{u.display_name}#{u.discriminator}'
+                for u in ctx.guild.members
+                if int(u.discriminator) >= discriminator
+            ])
+        elif selector == '<=':
+            p = Pages(ctx, entries=[
+                f'{u.display_name}#{u.discriminator}'
+                for u in ctx.guild.members
+                if int(u.discriminator) <= discriminator
+            ])
+        elif selector == '=':
+            p = Pages(ctx, entries=[
+                f'{u.display_name}#{u.discriminator}'
+                for u in ctx.guild.members
+                if int(u.discriminator) == discriminator
+            ])
+        else:
+            raise commands.BadArgument('Could not parse arguments')
+
+        if not p.entries:
+            return await ctx.send('No results found.')
+
+        await p.paginate()
+
+    @commands.command()
     async def help(self, ctx, option: str = None):
         """Help Command OwO"""
         color = 0xDEADBF
@@ -592,11 +703,12 @@ class General:
 
             embed.add_field(name="General",
                             value="`lmgtfy`, `cookie`, `flip`, `info`, `userinfo`, `serverinfo`, `channelinfo`, `urban`,"
-                                  " `avatar`, `qr`, `docs`, `vote`, `permissions`, `8ball`, `help`, `calc`, `crypto`, `duckduckgo`, `whois`, `memory`", inline=False)
+                                  " `avatar`, `qr`, `docs`, `vote`, `permissions`, `8ball`, `help`, `calc`, `crypto`, `duckduckgo`, `whois`, `memory`, "
+                                  "`discriminfo`, `discrim`", inline=False)
             embed.add_field(name="Audio", value="`play`, `skip`, `stop`, `now`, `queue`, `pause`, `volume`, `shuffle`, `repeat`, `find`, `disconnect`", inline=True)
             embed.add_field(name="Donator", value="`donate`, `redeem`, `upload`, `trapcard`")
             embed.add_field(name="Moderation",
-                            value="`kick`, `ban`, `massban`, `unban`, `rename`, `poll`, `purge`, `mute`, `unmute`", inline=False)
+                            value="`kick`, `ban`, `massban`, `unban`, `rename`, `poll`, `purge`, `mute`, `unmute`, `dehoist`", inline=False)
             embed.add_field(name="Roleplay", value="`card`")
             embed.add_field(name="IMGWelcomer", value="`imgwelcome`", inline=False)
             embed.add_field(name="Levels & Economy", value="`bank`, `register`, `profile`, `daily`, `rep`, `setdesc`, `transfer`, "
